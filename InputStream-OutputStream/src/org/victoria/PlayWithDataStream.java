897d39ad1e5ab0b98ea2b511c7181fe1a7a09a9f
Jenny Gan <jenny.gan89@gmail.com>
2023-06-08 17:12:13 +0200

    playing with data streams

---
+++ InputStream-OutputStream/src/org/victoria/PlayWithDataStream.java
@@ -0,0 +1,43 @@
+package org.victoria;
+
+import java.io.*;
+
+public class PlayWithDataStream {
+
+    /*
+    Read and write primitives directly from bytes
+     */
+
+    public static void main(String[] args) {
+        int[] ints = {1, 2, 3, 4, 5};
+
+        byte[] bytes = {};
+
+        try (ByteArrayOutputStream os = new ByteArrayOutputStream();
+             DataOutputStream dos = new DataOutputStream(os)) {
+
+            for (int integer : ints) {
+                dos.writeInt(integer);
+            }
+            dos.flush();
+            bytes = os.toByteArray();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("bytesLength = " + bytes.length);
+
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+             DataInputStream dis = new DataInputStream(bis)) {
+
+            for (int i = 0; i < 5; i++) {
+                int read = dis.readInt();
+                System.out.println("read = " + read);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
